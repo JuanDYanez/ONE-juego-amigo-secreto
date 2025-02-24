c0c63fc44757965b3ec16b148ec8d81fbe418e60
JuanDYanez <jdavid.front@gmail.com>
2025-02-23 20:05:55 -0500

    :sparkles: Función de agregar, sortear y limpiar
    Se finaliza el proyecto con las funcionalidades de agregar, sortear y limpiar, más algunos ajustes de UX/UI y estilos.

--- app.js
+++ app.js
@@ -1,35 +1,102 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = []
 
-const containerListaAmigos = document.getElementById("listaAmigos")
+const containerListaAmigos = document.getElementById("listaAmigos");
+const inputAmigo = document.getElementById("amigo");
+const containerAmigoSorteado = document.getElementById("resultado")
+const botonSortearAmigo = document.getElementById("botonSorteo")
 
-function agregarAmigo() {
-  let nuevoAmigo = document.getElementById("amigo").value.trim();
-  if (nuevoAmigo === '') return;
 
-  listaAmigos.push(nuevoAmigo)
 
-  const li = document.createElement("li")
-  li.textContent = nuevoAmigo
+function renderizarListaAmigos() {
+  containerListaAmigos.innerHTML = "";
 
-  const btnEliminar = document.createElement("button");
-  btnEliminar.textContent = "X";
-  btnEliminar.classList.add("delete-btn");
-  btnEliminar.onclick = () => {
-    containerListaAmigos.removeChild(li);
-    listaAmigos = listaAmigos.filter(amigo => amigo != nuevoAmigo)
-    console.log(listaAmigos);
-  }
+  listaAmigos.forEach((amigo, index) => {
+    const li = document.createElement("li");
+    li.textContent = amigo;
 
-  containerListaAmigos.appendChild(li)
+    const btnEliminar = document.createElement("button");
+    btnEliminar.textContent = "X";
+    btnEliminar.classList.add("delete-btn");
+    btnEliminar.onclick = () => eliminarAmigo(index);
 
-  li.appendChild(btnEliminar)
-
-  document.getElementById("amigo").value = ""
-  document.getElementById("amigo").focus();
-  
-  console.log(listaAmigos);
+    li.appendChild(btnEliminar);
+    containerListaAmigos.appendChild(li);
+  })
 }
 
+function agregarAmigo() {
+  let nuevoAmigo = inputAmigo.value.trim();
+  if (nuevoAmigo === '') return;
 
-document.querySelector('#amigo').addEventListener('keydown', e => e.key === 'Enter' ? agregarAmigo() : '')
+  nuevoAmigo = nuevoAmigo.charAt(0).toUpperCase() + nuevoAmigo.slice(1).toLowerCase();
+  
+  listaAmigos.push(nuevoAmigo);
+
+  console.log(listaAmigos);
+  
+  if (listaAmigos.length >= 1) {
+    habilitarBoton('botonSorteo');
+    document.getElementById('botonSorteo').classList.replace('button-draw-disabled', 'button-draw')
+  }
+
+  renderizarListaAmigos();
+  
+  inputAmigo.value = ""
+  inputAmigo.focus();
+}
+
+function eliminarAmigo(index) {
+  listaAmigos.splice(index, 1);
+  renderizarListaAmigos();
+}
+
+function limpiarLista() {
+  listaAmigos = [];
+  renderizarListaAmigos();
+  document.getElementById('resultado').innerHTML = "";
+}
+
+function sortearAmigo() {
+  if (listaAmigos.length === 1) {
+    deshabilitarBoton('botonSorteo')
+    document.getElementById('botonSorteo').classList.replace('button-draw', 'button-draw-disabled')
+  }
+
+  const randomIndex = Math.floor((Math.random() * listaAmigos.length-1) + 1)
+  
+  return randomIndex;
+}
+
+function renderizarAmigoSorteado() {
+  containerAmigoSorteado.innerHTML = "";
+
+  const amigoSorteado = listaAmigos.splice(sortearAmigo(), 1)[0]
+
+  const span = document.createElement("span");
+  const spanAmigo = document.createElement("span")
+
+  spanAmigo.textContent = amigoSorteado
+  spanAmigo.classList.add("friend-result-list")
+
+  span.textContent = `El amigo secreto es:`;
+
+  span.classList.add("text-result-list");
+
+  containerAmigoSorteado.appendChild(span);
+  containerAmigoSorteado.appendChild(spanAmigo);
+
+  renderizarListaAmigos();
+}
+
+function habilitarBoton(id) {
+  document.getElementById(id).removeAttribute('disabled')
+  document.getElementById(id).classList.replace('button-draw-disabled', 'button-draw')
+}
+
+function deshabilitarBoton(id) {
+  document.getElementById(id).setAttribute('disabled', true)
+}
+
+inputAmigo.addEventListener('keydown', e => {
+  if (e.key === 'Enter') agregarAmigo();
+});
